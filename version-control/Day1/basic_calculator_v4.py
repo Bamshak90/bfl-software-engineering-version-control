009b3943bc271d4595b33c2c294903e607c90e29
Bamshak90 <lilmarkhat@gmail.com>
2025-07-14 15:21:00 +0100

    Added changes to the basic_calculator_v4 file for easy print message

--- version-control/Day1/basic_calculator_v4.py
+++ version-control/Day1/basic_calculator_v4.py
@@ -3,4 +3,4 @@ value_two = input("Enter your second value: \n")
 
 value_sum = float(value_one)  + float(value_two)
 
-print("The sum is: ", value_sum)
+print(f"The sum of {value_one} and sum of {value_two}: ", value_sum)
